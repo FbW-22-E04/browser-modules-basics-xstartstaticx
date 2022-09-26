31bfa802a9c21a125e50443582ef8f0188272e3e
xstartstaticx <daniel.reiller@icloud.com>
2022-09-26 12:43:24 +0200

    finished exercise

---
+++ modules/index.js
@@ -0,0 +1,7 @@
+import { clickImages } from "./click.js";
+import { hoverHeader } from "./hover.js";
+import { scrollWindow as scroll } from "./scroll.js";
+
+clickImages();
+hoverHeader();
+scroll();
